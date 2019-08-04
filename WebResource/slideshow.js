d7f10c00ff6cfd673cf6d3ef13e338873967060e
Alex <alster551@gmail.com>
2019-08-05 00:52:20 +0100

    Changes to homepage

---
+++ WebResource/slideshow.js
@@ -0,0 +1,28 @@
+var files = [["ACLS001.png", "ACLS001","com.alsterdev.acls001"], ["ACLS002.png", "ACLS002","com.alsterdev.acls002"]];
+
+var currentInt;
+
+slideshow();
+
+function generateRandomNumber() {
+	var min = 0;
+	var max = files.length - 1;
+	randInt = Math.floor(Math.random() * (max - min + 1)) + min;
+	if (randInt == currentInt) {
+		generateRandomNumber();
+	}
+}
+
+function slideshow(){
+	generateRandomNumber();
+	currentInt = randInt;
+	imgSrc = "featuredBannerImages/" + files[randInt][0];
+	imgName = files[randInt][1];
+	imgURL = "sileo://package/" + files[randInt][2];
+	document.getElementById("slideshowImg").src = imgSrc;
+	document.getElementById("slideshowName").innerHTML = imgName;
+	document.getElementById("slideshowURL").href = imgURL;
+	
+}
+
+setInterval(slideshow, 5000);
